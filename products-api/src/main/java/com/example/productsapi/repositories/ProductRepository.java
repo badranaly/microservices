b614b87490b07782023b97becc81cb6e1be4b9df
Aly Badran <aly2cool@hotmail.com>
2018-06-11 21:37:40 -0400

    sql using 'c'

--- products-api/src/main/java/com/example/productsapi/repositories/ProductRepository.java
+++ products-api/src/main/java/com/example/productsapi/repositories/ProductRepository.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    @Query("SELECT * FROM PRODUCTS WHERE PRODUCT_CATEGORY = category")
-    Product findAllByCategory();
+    @Query("SELECT c FROM PRODUCTS c WHERE PRODUCT_CATEGORY = :category")
+    List<Product> findAllByCategory(@Param("category") String category);
 }
 
