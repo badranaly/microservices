90ba8550ff2876b6f6126a5278ac6a11321d6ebd
Aly Badran <aly2cool@hotmail.com>
2018-06-12 16:14:21 -0400

    added image seeds

--- products-api/src/main/resources/db/migration/afterMigrate.sql
+++ products-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,8 +1,8 @@
 INSERT INTO PRODUCTS
-(PRODUCT_NAME, PRODUCT_DESCRIPTION, PRODUCT_CATEGORY, PRODUCT_PRICE, USER_BOUGHT_ID, USER_SELLING_ID)
+(PRODUCT_NAME, PRODUCT_DESCRIPTION, PRODUCT_CATEGORY, PRODUCT_PRICE, PRODUCT_IMAGE, USER_BOUGHT_ID, USER_SELLING_ID)
    VALUES
-('mycar','runs beautifully','vehicles','5000', 1, 2),
-('my phone','works sometimes','electronics','700', 1, 2),
-('my couch','more like a char','misc','2000', 1, 2),
-('my sandwich','half eaten','misc','30', 1, 2),
-('megatron','lot of fun','toys','3500', 1, 2);
+('mycar','runs beautifully','vehicles','5000', 'https://www.youtube.com/watch?v=b1CNbzJSOfg', 1, 2),
+('my phone','works sometimes','electronics','700', 'https://imgur.com/gallery/0Xek2/comment/290341227', 1, 2),
+('my couch','more like a char','misc','2000', 'https://www.allthingsthrifty.com/bad-ideas-by-brooke-never-paint-a-couch/', 1, 2),
+('my sandwich','half eaten','misc','30', 'https://www.flickr.com/photos/toohotty/2475173954', 1, 2),
+('megatron','lot of fun','toys','3500', 'https://www.sideshowtoy.com/collectibles/transformers-megatron-prime-1-studio-9031411/', 1, 2);
