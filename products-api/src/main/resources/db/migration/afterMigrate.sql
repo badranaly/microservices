5615e68446b921bd97106d002cf9402a20274464
Aly Badran <aly2cool@hotmail.com>
2018-06-12 17:47:17 -0400

    updated image seeds

--- products-api/src/main/resources/db/migration/afterMigrate.sql
+++ products-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,8 +1,8 @@
 INSERT INTO PRODUCTS
 (PRODUCT_NAME, PRODUCT_DESCRIPTION, PRODUCT_CATEGORY, PRODUCT_PRICE, PRODUCT_IMAGE, USER_BOUGHT_ID, USER_SELLING_ID)
    VALUES
-('mycar','runs beautifully','vehicles','5000', 'https://www.youtube.com/watch?v=b1CNbzJSOfg', 1, 2),
-('my phone','works sometimes','electronics','700', 'https://imgur.com/gallery/0Xek2/comment/290341227', 1, 2),
-('my couch','more like a char','misc','2000', 'https://www.allthingsthrifty.com/bad-ideas-by-brooke-never-paint-a-couch/', 1, 2),
-('my sandwich','half eaten','misc','30', 'https://www.flickr.com/photos/toohotty/2475173954', 1, 2),
-('megatron','lot of fun','toys','3500', 'https://www.sideshowtoy.com/collectibles/transformers-megatron-prime-1-studio-9031411/', 1, 2);
+('mycar','runs beautifully','vehicles','5000', 'https://i.ytimg.com/vi/b1CNbzJSOfg/hqdefault.jpg', 1, 2),
+('my phone','works sometimes','electronics','700', 'https://news-cdn.softpedia.com/images/fitted/340x180/Nokia-3310-Doesn-t-Bend-but-It-Does-Blend-Video.jpg', 1, 2),
+('my couch','more like a char','misc','2000', 'https://www.allthingsthrifty.com/wp-content/uploads/2014/04/never-paint-a-couch.jpg', 1, 2),
+('my sandwich','half eaten','misc','30', 'https://previews.123rf.com/images/am3liawong/am3liawong1403/am3liawong140300017/26632951-half-eaten-sandwich.jpg', 1, 2),
+('megatron','lot of fun','toys','3500', 'https://www.sideshowtoy.com/wp-content/uploads/2017/09/transformers-beast-wars-magatron-statue-prime1-studio-feature-903141-1.jpg', 1, 2);
