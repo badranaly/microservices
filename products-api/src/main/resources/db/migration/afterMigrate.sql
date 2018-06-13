101a4b0e1e4ef614b5a81a87f0d36b2516ff65c1
Aly Badran <aly2cool@hotmail.com>
2018-06-12 21:34:05 -0400

    added more seeds

--- products-api/src/main/resources/db/migration/afterMigrate.sql
+++ products-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,8 +1,16 @@
 INSERT INTO PRODUCTS
 (PRODUCT_NAME, PRODUCT_DESCRIPTION, PRODUCT_CATEGORY, PRODUCT_PRICE, PRODUCT_IMAGE, USER_BOUGHT_ID, USER_SELLING_ID)
    VALUES
-('mycar','runs beautifully','vehicles','5000', 'https://i.ytimg.com/vi/b1CNbzJSOfg/hqdefault.jpg', 1, 2),
-('my phone','works sometimes','electronics','700', 'https://news-cdn.softpedia.com/images/fitted/340x180/Nokia-3310-Doesn-t-Bend-but-It-Does-Blend-Video.jpg', 1, 2),
-('my couch','more like a char','misc','2000', 'https://www.allthingsthrifty.com/wp-content/uploads/2014/04/never-paint-a-couch.jpg', 1, 2),
-('my sandwich','half eaten','misc','30', 'https://previews.123rf.com/images/am3liawong/am3liawong1403/am3liawong140300017/26632951-half-eaten-sandwich.jpg', 1, 2),
-('megatron','lot of fun','toys','3500', 'https://www.sideshowtoy.com/wp-content/uploads/2017/09/transformers-beast-wars-magatron-statue-prime1-studio-feature-903141-1.jpg', 1, 2);
+('mycar','runs beautifully, needs some minor work, but other than that its a steal','vehicles','5000', 'https://i.ytimg.com/vi/b1CNbzJSOfg/hqdefault.jpg', 1, 2),
+('my phone','works sometimes, prevents user from getting shot with bullet proof casing designed by nokia','electronics','700', 'https://news-cdn.softpedia.com/images/fitted/340x180/Nokia-3310-Doesn-t-Bend-but-It-Does-Blend-Video.jpg', 1, 2),
+('my couch','more like a chair, color is very appealing to guests','misc','2000', 'https://www.allthingsthrifty.com/wp-content/uploads/2014/04/never-paint-a-couch.jpg', 1, 2),
+('my sandwich','half eaten sandwich but tastes amazing.','misc','30', 'https://previews.123rf.com/images/am3liawong/am3liawong1403/am3liawong140300017/26632951-half-eaten-sandwich.jpg', 1, 2),
+('megatron','lot of fun...soo much fun like omg','toys','3500', 'https://www.sideshowtoy.com/wp-content/uploads/2017/09/transformers-beast-wars-magatron-statue-prime1-studio-feature-903141-1.jpg', 1, 2);
+('headphones','listen to life in this amazing headphones','electronics','200', 'https://slimages.macysassets.com/is/image/MCY/products/3/optimized/8100993_fpx.tif?op_sharpen=1&wid=500&hei=613&fit=fit,1&$filtersm$', 1, 2);
+('Mercedes','selling my car because its too expensive to keep','vehicles','5000', 'https://www.mercedes-benz.com/wp-content/uploads/sites/3/2017/12/00-mercedes-benz-mbsocialcar-glc-350-e-4matic-x-253-calendar-june-2018-2560x1440-848x477.jpg', 1, 2);
+('toolkit for everything','fix anything including your life with these tools','tools','150', 'https://www.growly.io/wp-content/uploads/2016/08/15-must-have-tools-for-small-business.jpg', 1, 2);
+
+
+
+
+
